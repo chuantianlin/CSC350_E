921f48de464b3ec8dd550db934345ac6bfd0ec92
chuantianlin <45149972+chuantianlin@users.noreply.github.com>
2020-04-15 02:18:21 -0400

    Update Database_design.sql

--- Database_design.sql
+++ Database_design.sql
@@ -29,7 +29,59 @@ FOREIGN KEY (CourseNo) REFERENCES classes(CourseNo),
 FOREIGN KEY (CourseNo) REFERENCES class_schedule(CourseNo)
 );
 
-
+insert into   classes value("CSC101",'4');
+insert into   classes value("CSC110",'4');
+insert into   classes value("CSC111",'5');
+insert into   classes value("CSC210",'5');
+insert into   classes value("CSC211",'5');
+insert into   classes value("CSC215",'4');
+insert into   classes value("CSC230",'6');
+insert into   classes value("CSC231",'4');
+insert into   classes value("CSC310",'4');
+insert into   classes value("CSC330",'4');
+insert into   classes value("CSC331",'5');
+insert into   classes value("CSC350",'5');
+insert into   classes value("CSC410",'4');
+insert into   classes value("CSC430",'4');
+insert into   classes value("CSC450",'4');
+insert into   classes value("CSC470",'5');
+insert into  classes value("CIS100",'4');
+insert into   classes value("CIS115",'4');
+insert into   classes value("CIS120",'3');
+insert into   classes value("CIS140",'3');
+insert into   classes value("CIS155",'5');
+insert into   classes value("CIS160",'3');
+insert into   classes value("CIS165",'4');
+insert into   classes value("CIS180",'4');
+insert into   classes value("CIS200",'4');
+insert into   classes value("CIS207",'6');
+insert into   classes value("CIS220",'4');
+insert into   classes value("CIS235",'5');
+insert into   classes value("CIS255",'6');
+insert into   classes value("CIS280",'4');
+insert into   classes value("CIS316",'4');
+insert into   classes value("CIS317",'4');
+insert into   classes value("CIS325",'4');
+insert into   classes value("CIS335",'5');
+insert into   classes value("CIS345",'5');
+insert into   classes value("CIS359",'4');
+insert into   classes value("CIS362",'4');
+insert into   classes value("CIS364",'4');
+insert into   classes value("CIS365",'5');
+insert into   classes value("CIS370",'4');
+insert into   classes value("CIS385",'4');
+insert into   classes value("CIS390",'4');
+insert into   classes value("CIS420",'5');
+insert into   classes value("CIS440",'4');
+insert into   classes value("CIS445",'5');
+insert into   classes value("CIS455",'5');
+insert into   classes value("CIS459",'5');
+insert into   classes value("CIS465",'10');
+insert into   classes value("CIS475",'5');
+insert into   classes value("CIS480",'3');
+insert into   classes value("CIS485",'4');
+insert into   classes value("CIS490",'4');
+insert into   classes value("CIS495",'4');
 
 
 
