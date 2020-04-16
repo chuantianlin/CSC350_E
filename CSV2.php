fed4e77038e26453f88e845ba414dcdba72a93ed
LetsGo-ops <57020544+LetsGo-ops@users.noreply.github.com>
2020-04-16 15:02:50 -0400

    Fixed

--- CSV2.php
+++ CSV2.php
@@ -3,70 +3,70 @@
 <head>
 <title>CVS READER</title>
 <link rel="stylesheet" href="style.css">
-<script src="https://phppot.com/wp-content/uploads/2018/02/csv-to-database.jpg"></script>
 </head>
 <body>
   <h1>Schedule Program</h1>
+  <div class="thisImg">
+    <img src="bmcc4.png" height="150" width="150" alt="">
+  </div>
   <form class="form-horizontal" action="" method="post" name="uploadCSV"
       enctype="multipart/form-data">
       <div class="input-row">
           <label class="inputTitle">Courses CSV File:</label>
-          <input type="file" name="file" id="fInput" accept=".csv">
+          <input type="file" name="file" id="fImput" accept=".csv">
             </br>
           <label class="inputTitle">Rooms CSV File:</label>
-          <input type="file" name="Rfile"  id="sInput" accept=".csv">
+          <input type="file" name="Rfile"  id="sImput" accept=".csv">
                 </br>
           <button id="b" type="submit" name="import">Import</button>
           <br />
       </div>
-      <div id="labelError"></div>
-  </form>
-
-<?php
-$conn = mysqli_connect("localhost", "root", "", "class_arrangement");
+    </form>
 
 
-if (isset($_POST["import"])) {
-  //echo"hello";
-
-    $fileName = $_FILES["file"]["tmp_name"];
+  <?php
+  $conn = mysqli_connect("localhost", "root", "", "file");
 
 
-    if ($_FILES["file"]["size"] > 0) {
+  if (isset($_POST["import"])) {
+    //echo"hello";
+
+      $fileName = $_FILES["file"]["tmp_name"];
 
 
-        $myQ="Delete from course_Info";
-        $result = mysqli_query($conn, $myQ);
-
-        $file = fopen($fileName, "r");
-
-        while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
-            $sqlInsert = "INSERT into course_Info
-                   values ('" . $column[0] . "','" . $column[1] . "')";
-            $result = mysqli_query($conn, $sqlInsert);
+      if ($_FILES["file"]["size"] > 0) {
 
 
-        }
-    }
+          $myQ="Delete from course_Info";
+          $result = mysqli_query($conn, $myQ);
 
-      $handle = $_FILES["Rfile"]["tmp_name"];
-    if ($_FILES["Rfile"]["size"] > 0) {
+          $file = fopen($fileName, "r");
 
-        //  echo"HI";
-        $myQ="Delete from roomnum";
-        $esult = mysqli_query($conn, $myQ);
-
-        $file = fopen($handle, "r");
-
-        while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
-            $sqlInsert = "INSERT into roomnum
-                   values ('" . $column[0] . "')";
-            $result = mysqli_query($conn, $sqlInsert);
+          while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
+              $sqlInsert = "INSERT into course_Info
+                     values ('" . $column[0] . "','" . $column[1] . "')";
+              $result = mysqli_query($conn, $sqlInsert);
 
 
-        }
+          }
       }
-}
-?>
+
+        $handle = $_FILES["Rfile"]["tmp_name"];
+      if ($_FILES["Rfile"]["size"] > 0) {
+
+          //  echo"HI";
+          $myQ="Delete from roomnum";
+          $esult = mysqli_query($conn, $myQ);
+
+          $file = fopen($handle, "r");
+
+          while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
+              $sqlInsert = "INSERT into roomnum
+                     values ('" . $column[0] . "')";
+              $result = mysqli_query($conn, $sqlInsert);
+          }
+        }
+  }
+  ?>
 </body>
 </html>
