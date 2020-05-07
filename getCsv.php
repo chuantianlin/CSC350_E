9da93f9c80dcffe310d4052d58da85afef0607dc
chuantianlin <45149972+chuantianlin@users.noreply.github.com>
2020-05-07 19:24:35 -0400

    Update getCsv.php

--- getCsv.php
+++ getCsv.php
@@ -449,4 +449,10 @@ return str_pad($hours,2,"0",STR_PAD_LEFT).str_pad($minutes,2,"0",STR_PAD_LEFT).s
 
 }
 }
+echo'</br></br></br></br></br></br></br><hr>
+
+<a href="https://www.facebook.com/chuantian.lin" class="fa fa-facebook"></a>
+<a href="mailto:llcttt@gmail.com" class="fa fa-google"></a>
+<a href="https://www.instagram.com/null_lct/" class="fa fa-instagram"></a>';
+
 ?>
