a649c770693fdce1950c1406d73f7329675d4eb1
LetsGo-ops <57020544+LetsGo-ops@users.noreply.github.com>
2020-05-16 21:55:06 -0400

    last added

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> CIS Department</title>
+    <link rel="stylesheet" href="index.css">
+  </head>
+  <body>
+    <div class="">
+      <div class="image">
+          <a class="link" href="https://www.bmcc.cuny.edu/" target="_blank"><img src="bmcc4.png" alt="BMCC" height="150" height="150"></a>
+      </div>
+      <div class="intro">
+        <h1>Term Project</h1>
+        <p><h3>This project will schedule sections of courses for CIS department.</h3></p>
+      </div>
+      <div class="social">
+        <a href="mailto:llcttt@gmail.com "><img id="gmail" src="gmail.png" alt="gmail" height="30" height="30"></a>
+        <a href="mailto:william.zhicay@stu.bmcc.cuny.edu"><img src="outlook.png" alt="outlook" height="30" height="30"></a>
+      </div>
+    </div>
+  <div class="row">
+    <div class="col">
+      <a class="link" href="getCsv.php" target="_blank"><img  class="zoom" id="db"src="db.png" alt="" width="200" height="200"></a><br>
+      <button class="button" type="button" name="button"><a class="link" href="getCsv.php" target="_blank">Import CSV Files</a></button>
+    </div>
+    <div class="col">
+      <a class="link" href="table.php" target="_blank"><img  class="zoom" id="sche"src="schedule.png" width="225" height="200" alt=""></a><br>
+      <button id="leftButton2" class="button" type="button" name="button"><a class="link" href="table.php" target="_blank">Go To Table</a></button>
+    </div>
+    <div class="col">
+      <a class="link" href=" " target="_blank"><img class="zoom" id="sche"src="list.png" width="225" height="200" alt=""></a><br> <!–– needs to be linked to a file-->
+      <button id="leftButton3" class="button" type="button" name="button"><a class="link" href=" " target="_blank">View List </a></button>
+    </div>
+  </div>
+
+  </body>
+</html>
